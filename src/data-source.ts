e20bc1bd4b471598ac686b29e1ae0a67264a8ec6
Ricardo Martins <hikfm2@gmail.com>
2023-02-08 16:39:13 -0300

    feat: documentation routes

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,4 +1,6 @@
+import "reflect-metadata";
 import { DataSource } from "typeorm";
+import path from "path";
 import "dotenv/config";
 
 const AppDataSource = new DataSource(
@@ -9,12 +11,34 @@ const AppDataSource = new DataSource(
         synchronize: true,
         entities: ["src/entities/*.ts"],
       }
+    : process.env.NODE_ENV === "production"
+    ? {
+        type: "postgres",
+        url:
+          process.env.NODE_ENV === "production"
+            ? process.env.DATABASE_URL
+            : `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}`,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+        synchronize: process.env.NODE_ENV === "production" ? false : true,
+        logging: true,
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/entities/*.js"]
+            : [path.join(__dirname, "./entities/*.{js,ts}")],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/src/migrations/*.js"]
+            : [path.join(__dirname, "./migrations/*.{js,ts}")],
+      }
     : {
         type: "postgres",
-        host: process.env.HOST,
+        host: process.env.PGHOST,
         port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
         database: process.env.POSTGRES_DB,
         logging: true,
         synchronize: false,
