d664d32c16e89d5d26a20bdb81260258909d1eee
Ricardo Martins <hikfm2@gmail.com>
2023-02-08 19:51:22 -0300

    feat: railway app configuration

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import path from "path";
 import "dotenv/config";
 
 const AppDataSource = new DataSource(
@@ -11,26 +10,12 @@ const AppDataSource = new DataSource(
         synchronize: true,
         entities: ["src/entities/*.ts"],
       }
-    : process.env.NODE_ENV === "production"
-    ? {
-        type: "postgres",
-        url: process.env.DATABASE_URL,
-        host: process.env.PGHOST,
-        port: Number(process.env.PGPORT),
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.POSTGRES_DB,
-        synchronize: false ,
-        logging: true,
-        entities: ["dist/src/entities/*.js"],
-        migrations: ["dist/src/migrations/*.js"]
-      }
     : {
         type: "postgres",
-        host: process.env.PGHOST,
-        port: 5432,
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
         logging: true,
         synchronize: false,
