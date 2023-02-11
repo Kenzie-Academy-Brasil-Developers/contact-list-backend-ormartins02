3fd9b5b69aee2d6830aca22f5c0b1231efd261b8
Ricardo Martins <hikfm2@gmail.com>
2023-02-11 19:21:54 -0300

    feat: enviroments modified

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import handleErrorMiddleware from "./middlewares/handleError.middleware";
 import { contactRoutes } from "./Routes/contact.Routes";
 import { sessionRoutes } from "./Routes/session.routes";
 import { userRoutes } from "./Routes/user.Routes";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/users", userRoutes)
 app.use("/session", sessionRoutes)
--- src/data-source.ts
+++ src/data-source.ts
@@ -12,6 +12,7 @@ const AppDataSource = new DataSource(
       }
     : {
         type: "postgres",
+        url: process.env.DATABASE_URL,
         host: process.env.POSTGRES_HOST,
         port: Number(process.env.POSTGRES_PORT),
         username: process.env.POSTGRES_USER,
